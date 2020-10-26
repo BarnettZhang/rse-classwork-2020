848500e38327dfa29d66400170f00eebfeb19f5c
BarnettZhang <zcbezzh@ucl.ac.uk>
2020-10-26 02:20:50 +0000

    Finished week04 exercise

--- week04/quakes.py
+++ week04/quakes.py
@@ -2,8 +2,26 @@
 
 # At the top of the file, import any libraries you will use.
 # import ...
+import requests
+import json
+quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+                      params={
+                          'starttime': "2000-01-01",
+                          "maxlatitude": "58.723",
+                          "minlatitude": "50.008",
+                          "maxlongitude": "1.67",
+                          "minlongitude": "-9.756",
+                          "minmagnitude": "1",
+                          "endtime": "2018-10-11",
+                          "orderby": "time-asc"}
+                      )
+with open('quakes.txt', 'w') as output:
+    output.write(quakes.text)
 
-# If you want, you can define some functions to help organise your code.
+with open('quakes.txt', 'r') as input:
+    quakeUK = json.loads(input.read())
+
+    # If you want, you can define some functions to help organise your code.
 # def helper_function(argument_1, argument_2):
 #   ...
 
@@ -12,6 +30,12 @@
 # You can run the file with `python quakes.py` from this directory.
 if __name__ == "__main__":
     # ...do things here to find the results...
+    max_magnitude = 0
+    coords = []
+    for item in quakeUK['features']:
+        if item["properties"]["mag"] >= max_magnitude:
+            max_magnitude = item["properties"]["mag"]
+            coords = item['geometry']['coordinates']
 
     # The lines below assume that the results are stored in variables
     # named max_magnitude and coords, but you can change that.
