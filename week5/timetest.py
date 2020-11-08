69bc94127684ae7081957c4981152f4b997073c6
BarnettZhang <zcbezzh@ucl.ac.uk>
2020-11-08 02:17:01 +0000

    Week4's quake exercise

---
+++ week5/timetest.py
@@ -0,0 +1,25 @@
+from datetimetest import compute_overlap_time, time_range
+import pytest
+
+
+@pytest.mark.parametrize("first_range, second_range, expected_overlap",
+[(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+  time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
+  [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]),
+  (time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"),
+  time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60),
+  []),
+  (time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00", 3, 900),
+  time_range("2010-01-12 10:40:00", "2010-01-12 11:20:00", 2, 120),
+  [("2010-01-12 10:40:00","2010-01-12 10:50:00"), ("2010-01-12 11:05:00", "2010-01-12 11:20:00")]),
+  (time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"),
+  time_range("2010-01-12 11:00:00", "2010-01-12 12:45:00"),
+  [])
+])
+def test_time_range_overlap(first_range, second_range, expected_overlap):
+    assert compute_overlap_time(first_range, second_range) == expected_overlap
+
+def test_negative_time_range():
+    with pytest.raises(ValueError) as e:
+        time_range("2010-01-12 10:00:00", "2010-01-12 09:30:00")
+        assert e.match('The end of the time range has to come strictly after its start.')
